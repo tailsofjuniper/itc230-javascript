044e575570d41534bba49c40b81e068b0c8969d6
tailsofjuniper <tailsofjuniper@gmail.com>
2020-04-23 22:30:58 -0700

    commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const http = require("http");  
+const http = require("http"); 
 http.createServer((req,res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola');
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
 }).listen(process.env.PORT || 3000);
