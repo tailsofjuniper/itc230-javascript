a391c88455cfdfd2d4dac896c7e6e4e215c7c30e
tailsofjuniper <tailsofjuniper@gmail.com>
2020-04-26 20:40:37 -0700

    commit

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 // let items = [
 //     "red", "orange", "yellow", "green", "blue"
 // ]
-let bicycles = [
+var bicycle = [
     { make : 'Trek', type : 'ATB', colour : 'red', wheel : '27.5in' },
     { make : 'Specialized', type : 'ATB', colour : 'green', wheel : '29in' },
     { make : 'Colnago', type : 'RD', colour : 'blue', wheel : '700c' },
     { make : 'Look', type : 'RD', colour : 'red', wheel : '700c' },
     { make : 'Colnago', type : 'RD', colour : 'yellow', wheel : '700c' }
     ];
-exports.bicycles = bicycles
+var bicycles = bicycle.length;
 
-exports.getAll = (length) => {
-    return bicycles[length]
-}
+// const getAll = () => {
+//     return bicycle;
+// };
 
-console.log(bicycles.length)
-console.log(bicycles[1])
+// console.log(bicycles.length)
+// console.log(bicycles[1])
