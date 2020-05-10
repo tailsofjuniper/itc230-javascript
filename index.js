8659d8d9ffa02bc0f5620fcaeb189f72d1f49a8b
tailsofjuniper <tailsofjuniper@gmail.com>
2020-05-10 16:00:14 -0700

    commit

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-// const app = express();
+const all = require("./data.js");
+const bicycles = require("./data.js");
+bicycles.bicycle;
+all.getAll();var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - not found');
+});
+
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - not found')});
+
+app.listen(app.get('port'), function(){
+  console.log('Express server started');
+});
+
+
+// const app = express;
 // app,engine('handlebars', hbars({defaultLayout: false}));
 // app.set('view engine', 'handlebars');
 // app.set('port', process.env.PORT || 3000);
 // app.use(bodyParser.urlencoded({extended: true}));
-
-const all = require("./data.js");
-const bicycles = require("./data.js");
-bicycles.bicycle;
-all.getAll();
-const http = require("http"); 
-http.createServer((req,res) => {
-  const path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-  // app.get('/', function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("Array length: " + bicycles.length);
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("Hello, I'm a database developer.");
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end("Not found");
-      break;
-  }
-  
-    // app.get('/', (req, res) => {
-    //   res.render('home', {bicycles.getAll()})
-    // } 
-    
-    // app.get('/detail', (req, res) => {
-    //   res.render('detail', {bicycles: req.query.item});
-    // }
-    // app.use((req, res) => {
-    //   res.type('text/plain');
-    //   res.status(404);
-    //   res.send('404 Error - Not Found');
-    // }
-
-}).listen(process.env.PORT || 3000);
-//     app.listen(app.get('port'), () => {
-//     console.log('Express startup");')
-// });
-// })
+// 
+// const all = require("./data.js");
+// const bicycles = require("./data.js");
+// bicycles.bicycle;
+// all.getAll();
---
+++ trash.js
@@ -0,0 +1,47 @@
+// const app = express;
+// app,engine('handlebars', hbars({defaultLayout: false}));
+// app.set('view engine', 'handlebars');
+// app.set('port', process.env.PORT || 3000);
+// app.use(bodyParser.urlencoded({extended: true}));
+// 
+// const all = require("./data.js");
+// const bicycles = require("./data.js");
+// bicycles.bicycle;
+// all.getAll();
+// const http = require("http"); 
+// http.createServer((req,res) => {
+//   const path = req.url.toLowerCase();
+//   switch(path) {
+//     case '/':
+//   // app.get('/', function (req, res) {
+//       res.writeHead(200, {'Content-Type': 'text/plain'});
+//       res.end("Array length: " + bicycles.length);
+//       break;
+//     case '/about':
+//       res.writeHead(200, {'Content-Type': 'text/plain'});
+//       res.end("Hello, I'm a database developer.");
+//       break;
+//     default:
+//       res.writeHead(404, {'Content-Type': 'text/plain'});
+//       res.end("Not found");
+//       break;
+//   }
+  
+//     // app.get('/', (req, res) => {
+//     //   res.render('home', {bicycles.getAll()})
+//     // } 
+    
+//     // app.get('/detail', (req, res) => {
+//     //   res.render('detail', {bicycles: req.query.item});
+//     // }
+//     // app.use((req, res) => {
+//     //   res.type('text/plain');
+//     //   res.status(404);
+//     //   res.send('404 Error - Not Found');
+//     // }
+
+// }).listen(process.env.PORT || 3000);
+//     app.listen(app.get('port'), () => {
+//     console.log('Express startup");')
+// });
+// })
